4374ade2e34bcc8b99eda93857f3c9c28165d352
Apocryphon@06 <hrithikchandrashekar@gmail.com>
2025-10-22 18:54:31 +0530

    updated readme

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,78 +1,267 @@
-import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
+import React, { useEffect, useState } from 'react'
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const productsList = [
+  { id: 1, name: "Laptop", price: 500, qty: 1 },
+  { id: 2, name: "Smartphone", price: 300, qty: 1 },
+  { id: 3, name: "Headphones", price: 100, qty: 1 },
+  { id: 4, name: "Smartwatch", price: 150, qty: 1 },
+];
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const freeGift = { id: 99, name: "Wireless Mouse", price: 0, qty: 1 };
 
-export default function Home() {
-  return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black`}
-    >
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the index.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs/pages/getting-started?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+const threshold = 1000;
+
+const calculateSubTotal = (myArray) => {
+
+  let subTotal = 0
+
+  myArray.map(item => {
+    subTotal += Math.round(item?.price * item?.qty)
+  })
+
+  return subTotal
+
 }
+
+
+const Home = () => {
+
+  const [subTotal, setSubTotal] = useState(0)
+  const [cartItems, setCartItems] = useState([])
+  const [products, setProducts] = useState(productsList)
+
+  const progress = Math.min((subTotal / threshold) * 100, 100);
+
+  const handleAddToCart = (prod) => {
+    let temp = [...cartItems]
+
+    let existingIndex = temp.findIndex(item => item?.id === prod.id)
+
+    if (existingIndex !== -1) {
+      temp[existingIndex].qty += prod.qty
+    }
+    else {
+      temp.push({
+        id: prod.id,
+        name: prod.name,
+        price: prod.price,
+        qty: prod.qty,
+      })
+    }
+
+    setCartItems(temp)
+  }
+
+  const handleCartQtyIncrement = (index) => {
+    let temp = [...cartItems]
+    temp[index] = {
+      ...temp[index],
+      qty: temp[index].qty + 1
+    }
+
+    setCartItems(temp)
+  }
+
+
+  const handleCartQtyDecrement = (index) => {
+    let temp = [...cartItems]
+
+    if (temp[index].qty === 1) {
+      temp.splice(index, 1)
+    }
+    else {
+      temp[index] = {
+        ...temp[index],
+        qty: temp[index].qty - 1
+      }
+    }
+
+    setCartItems(temp)
+  }
+
+  const handleProductQtyIncrement = (index) => {
+    let temp = [...products]
+    temp[index] = {
+      ...temp[index],
+      qty: temp[index].qty + 1
+    }
+
+    setProducts(temp)
+  }
+
+
+  const handleProductQtyDecrement = (index) => {
+    let temp = [...products]
+    temp[index] = {
+      ...temp[index],
+      qty: temp[index].qty - 1
+    }
+    setProducts(temp)
+  }
+
+  useEffect(() => {
+    if (cartItems?.length > 0) {
+      setSubTotal(calculateSubTotal(cartItems))
+    }
+    else {
+      setSubTotal(0)
+    }
+  }, [cartItems])
+
+  useEffect(() => {
+
+    let temp = [...cartItems]
+    const freeGiftIndex = temp.findIndex(item => item?.id === freeGift.id)
+
+    if (subTotal >= threshold && freeGiftIndex === -1) {
+      temp.push(freeGift)
+      setCartItems(temp)
+    }
+    else if (subTotal < threshold && freeGiftIndex !== -1) {
+      temp.splice(freeGiftIndex, 1)
+      setCartItems(temp)
+    }
+
+  }, [subTotal])
+
+  return (
+    <div className='flex flex-col w-full min-h-screen bg-[#f2f2f2] lg:p-10 p-6 lg:gap-6 gap-4'>
+
+      <h1 className='lg:text-2xl text-xl text-[#121212] font-bold'>Shopping Cart</h1>
+
+
+      <div className='flex flex-col lg:gap-6 gap-4 w-full'>
+
+        <h2 className='lg:text-xl text-lg text-[#121212] font-bold'>Products</h2>
+
+
+        <div className='flex items-center w-full overflow-x-auto snap-x snap-mandatory lg:gap-6 gap-4 pb-4'>
+
+          {
+            products.map((item, index) => (
+              <div className='lg:w-1/4 shrink-0 flex flex-col lg:p-6 p-4 lg:gap-4 gap-2 bg-white rounded-md shadow'>
+
+                <p className='lg:text-base text-sm text-[#121212]'>{item?.name}</p>
+                <p className='lg:text-base text-sm text-[#121212] font-semibold'>&#8377; {item?.price}</p>
+
+                <div className='flex items-center gap-4'>
+
+                  <button onClick={() => handleProductQtyDecrement(index)} disabled={item?.qty === 1} className='w-9 h-9 border border-[#808080] rounded-md lg:text-base text-sm disabled:cursor-not-allowed cursor-pointer disabled:opacity-50'>
+                    -
+                  </button>
+
+                  <span className='lg:text-base text-sm'>{item?.qty}</span>
+
+                  <button onClick={() => handleProductQtyIncrement(index)} className='w-9 h-9 border border-[#808080] rounded-md lg:text-base text-sm disabled:cursor-not-allowed cursor-pointer disabled:opacity-50'>
+                    +
+                  </button>
+
+                </div>
+
+                <button onClick={() => handleAddToCart(item)} className='py-2 px-4 bg-blue-500 text-white lg:text-base text-sm font-medium rounded-md cursor-pointer select-none'>
+                  Add to Cart
+                </button>
+
+              </div>
+            ))
+          }
+
+        </div>
+
+      </div>
+
+      <div className='flex flex-col lg:gap-6 gap-4 w-full'>
+
+        <h2 className='lg:text-xl text-lg text-[#121212] font-bold'>Cart Summary</h2>
+
+        <div className='flex flex-col lg:p-6 p-4 bg-white rounded-md shadow lg:gap-6 gap-4'>
+
+          <div className='flex items-center justify-between w-full pb-4 border-b border-[#121212]'>
+
+            <p className='lg:text-base text-sm font-semibold text-[#121212]'>Subtotal</p>
+            <p className='lg:text-base text-sm font-semibold text-[#121212]'>&#8377; {subTotal}</p>
+
+          </div>
+
+
+          {
+            (subTotal < threshold) ?
+              <div className='flex flex-col lg:p-6 p-4 bg-blue-50 gap-2 rounded-md'>
+
+                <p className='lg:text-base text-sm text-[#121212] font-medium'>Add <span className='font-semibold'>&#8377;{threshold - subTotal}</span> more to get a FREE Wirelss Mouse!</p>
+
+                <div className='w-full h-4 bg-slate-200 rounded-md-full relative overflow-hidden'>
+
+                  <div style={{ width: `${progress}%` }} className='h-full absolute bg-blue-500 top-0 left-0' />
+
+                </div>
+
+              </div> :
+              <p className='lg:text-base text-sm text-[#121212] font-medium'>You got a free Wireless Mouse!</p>
+          }
+
+        </div>
+
+      </div>
+
+
+      {
+        cartItems?.length > 0 ?
+          <div className='flex flex-col lg:gap-6 gap-4 w-full'>
+
+            <h2 className='lg:text-xl text-lg text-[#121212] font-bold'>Cart Items</h2>
+
+            <div className='flex flex-col lg:gap-4 gap-2'>
+              {
+                cartItems.map((item, index) => (
+                  <div className='flex items-center justify-between w-full lg:p-6 p-4 bg-white rounded-md shadow'>
+
+                    <div>
+                      <p className='lg:text-base text-sm font-semibold text-[#121212]'>{item?.name}</p>
+                      <p className='lg:text-base text-sm font-semibold text-[#12121280]'>&#8377; {item?.price} * {item?.qty} = {Math.round(item?.price * item?.qty)}</p>
+                    </div>
+
+
+                    {
+                      item?.id === freeGift.id ?
+                        <div className='py-2 px-4 bg-green-100 rounded-md text-green-700 text-sm font-semibold uppercase'>
+                          Free Gift
+                        </div> :
+                        <div className='flex items-center gap-4'>
+
+                          <button onClick={() => handleCartQtyDecrement(index)} className='w-9 h-9 bg-red-500 rounded-md text-white lg:text-base text-sm disabled:cursor-not-allowed cursor-pointer disabled:opacity-50'>
+                            -
+                          </button>
+
+                          <span className='lg:text-base text-sm'>{item?.qty}</span>
+
+                          <button onClick={() => handleCartQtyIncrement(index)} className='w-9 h-9 bg-green-500 rounded-md text-white lg:text-base text-sm disabled:cursor-not-allowed cursor-pointer disabled:opacity-50'>
+                            +
+                          </button>
+
+                        </div>
+                    }
+                  </div>
+                ))
+              }
+            </div>
+
+          </div> :
+          <div className='flex flex-col w-full lg:p-6 p-4 bg-white rounded-md min-h-40 shadow items-center justify-center'>
+
+            <h2 className='lg:text-base text-sm text-[#121212] font-medium'>Your cart is empty</h2>
+            <p className='lg:text-base text-sm'>Add some products to see them here!</p>
+
+          </div>
+      }
+
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Home
